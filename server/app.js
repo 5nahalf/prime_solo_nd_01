3871c60fb52405cd6e05bb27b1e1f2488146f933
5nahalf <5nahalf@comcast.net>
2015-05-07 14:16:37 -0500

    Just a test

--- server/app.js
+++ server/app.js
@@ -2,11 +2,12 @@ var express = require("express");
 var app = express();
 var path = require("path");
 app.set("port", (process.env.PORT || 5000));
+
 app.get("/*", function(request, response){
     var file = request.param[0] || "views/index.html";
     response.sendFile(path.join(__dirname, "./public", file));
-
 });
+
 app.listen(app.get("port"), function(){
     console.log("app is running on port", app.get("port"));
 });
